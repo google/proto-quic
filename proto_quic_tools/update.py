c9737ab7570e43adb15691f0e2e8dd9cb7a277c1
Jana Iyengar <jri@chromium.org>
2016-12-29 15:00:52 -0800

    Fixes scripts: - update and sync scripts now downloads build/linux/sysroot, and sysroot is used for building - cleanup is now additionally done before building locally during merge, to ensure that a working build is checked in

--- proto_quic_tools/update.py
+++ proto_quic_tools/update.py
@@ -17,9 +17,10 @@ import shutil
 import subprocess
 import sys
 
-usage = "export CHROME_ROOT=/path/to/chrome/src;export PROTO_QUIC_ROOT=/path/to/proto-quic; update.py"
+usage = "export CHROME_ROOT=/path/to/chrome/src;export PROTO_QUIC_ROOT=/path/to/proto-quic/src; update.py"
 chrome_root = os.environ.get('CHROME_ROOT')
 proto_quic_root = os.environ.get('PROTO_QUIC_ROOT')
+tools_dir = proto_quic_root + "/../proto_quic_tools"
 modified_files_dir = proto_quic_root + "/../modified_files"
 print "Running with chrome_root=", chrome_root, ", proto_quic_root=", proto_quic_root;
 if chrome_root == None or proto_quic_root == None:
@@ -113,12 +114,21 @@ def copy_modified_files():
 
 # Since proto-quic users do not use gclient sync, runs necessary parts of it.
 def sync():
-  command = "/" + proto_quic_root + "/third_party/binutils/download.py"
-  print "running", command;
+  command = tools_dir + "/sync.sh"
+  print "running", command
+  os.system(command)
+
+def cleanup():
+  command = tools_dir + "/cleanup.sh"
+  print "running", command
   os.system(command)
 
 
-sync()
+# In the functions called below, the sequence of copy, merge, copy, and cleanup attempts
+# to leave the workspace in exactly the state that someone checking out repo will see.
 copy_directories()
 merge_net_gypi()
 copy_modified_files()
+cleanup()
+# Finally sync pulls down files required for building which are not part of the repo.
+sync()
