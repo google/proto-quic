b40915f8c0cc191efdaf54273d1083c3626d21e0
Ryan Hamilton <rch@google.com>
2017-08-25 11:10:13 -0700

    not chrome root

--- proto_quic_tools/sync.sh
+++ proto_quic_tools/sync.sh
@@ -10,11 +10,6 @@ if [ ! -f "$PROTO_QUIC_ROOT/net/BUILD.gn" ]; then
    exit 1
 fi
 
-if [ ! -f "$CHROME_ROOT/net/BUILD.gn" ]; then
-   echo "CHROME_ROOT not set correctly."
-   exit 1
-fi
-
 echo "_____ running src/third_party/binutils/download.py"
 $PROTO_QUIC_ROOT/third_party/binutils/download.py
 echo "_____ running /usr/bin/python src/build/linux/sysroot_scripts/install-sysroot.py --running-as-hook"
