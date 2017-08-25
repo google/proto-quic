ec5909ba1ff089fb1ee352b2db25ed972a1b3c4c
Ryan Hamilton <rch@palladium.mtv.corp.google.com>
2017-08-25 10:46:22 -0700

    Modify proto_quic_tools/sync.sh to fail if CHROME_ROOT or PROTO_QUIC_ROOT are not set correctly.

--- proto_quic_tools/sync.sh
+++ proto_quic_tools/sync.sh
@@ -5,6 +5,16 @@ if [ "$PROTO_QUIC_ROOT" == "" ]; then
     exit 1
 fi
 
+if [ ! -f "$PROTO_QUIC_ROOT/net/BUILD.gn" ]; then
+   echo "PROTO_QUIC_ROOT not set correctly"
+   exit 1
+fi
+
+if [ ! -f "$CHROME_ROOT/net/BUILD.gn" ]; then
+   echo "CHROME_ROOT not set correctly"
+   exit 1
+fi
+
 echo "_____ running src/third_party/binutils/download.py"
 $PROTO_QUIC_ROOT/third_party/binutils/download.py
 echo "_____ running /usr/bin/python src/build/linux/sysroot_scripts/install-sysroot.py --running-as-hook"
